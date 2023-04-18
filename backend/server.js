54cf70997fc8b8371090f37438b7c4656cecff6c
Cal <calvin.m71@hotmail.com>
2023-04-17 17:24:19 -0700

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
-const app = express();
+const connectDB = require('./config/db');
 const users = require('./routes/userRoutes');
+const { errorHandler }= require('./middleware/errorMiddleware');
+
+//Connect to Database
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Hello'});
 });
 app.use('/api/users', users);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server starts on ${PORT}`));
