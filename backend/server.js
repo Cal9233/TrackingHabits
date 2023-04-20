726b45520a0f9ad7c135172d3f1b442799eaa8ca
Cal <calvin.m71@hotmail.com>
2023-04-20 15:37:21 -0700

    heroku

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
@@ -28,11 +29,11 @@ if(process.env.NODE_ENV === 'production'){
 
   // FIX: below code fixes app crashing on refresh in deployment
   app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+    res.sendFile(path.join(__dirname, '../', 'frontend', 'build', 'index.html'))
   })
 } else {
   app.get('/', (_, res) => {
-    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+    res.status(200).json({ message: 'Welcome to Habit Tracker API' })
   })
 }
 app.use(errorHandler);
